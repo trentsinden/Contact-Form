d742205d49102aea42d8bdff34efbed8d366d6d3
Trent Sinden <trentsinden@gmail.com>
2020-05-26 12:53:03 -0400

    completed contact page

---
+++ index.php
@@ -0,0 +1,23 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <main>
+        <p>Send Email</p>
+        <form class="contact-form" action="index.html" method="POST">
+            <input type="text" name="name" placeholder="Full name">
+            <input type="text" name="mail" placeholder="Your email">
+            <input type="text" name="subject" placeholder="Subject">
+            <textarea name="" placeholder="Message"></textarea>
+            <button type="submit" name="submit">Send Mail</button>
+        </form>
+    </main>
+</body>
+
+</html>
